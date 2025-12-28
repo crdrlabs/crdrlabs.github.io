17cfeba95cfbd5218b8a91291e8095cf4ab5f002
James Corey <jmc-git@neniam.net>
2025-12-28 03:03:29 -0800

    add qr to links

--- bizcard/gen_qr_code.sh
+++ bizcard/gen_qr_code.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 #qrencode -t SVG -o qrcode.svg "https://crdrlabs.github.io/pubdocs/links.html"
-qrencode -v 2 -t SVG -m 0 -o qrcode.svg "https://crdrlabs.org/links.html"
+qrencode -v 2 -t SVG -m 0 -o qr_links_bw.svg "https://crdrlabs.org/links.html"
+qrencode -v 2 -t SVG -m 0 \
+    --foreground=dbc29bff \
+    --background=1c1c1c00 \
+    -o qr_links_clear.svg "https://crdrlabs.org/links.html"
 #qrencode -v 1 -t SVG -o qrcode.svg "WWW.CRDRLABS.ORG"
